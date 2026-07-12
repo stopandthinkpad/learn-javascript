5b77b43ddfffea33fbedc5286c8761aec2dfc2bc
stopandthinkpad <298364173+stopandthinkpad@users.noreply.github.com>
2026-07-12 19:07:45 -0400

    update counter app with conditional styling and some streamlined functions

--- mini-projects/project4_counterapp/index.js
+++ mini-projects/project4_counterapp/index.js
@@ -3,40 +3,55 @@ const increase = document.getElementById("increase")
 const decrease = document.getElementById("decrease")
 const reset = document.getElementById("reset")
 
-increase.addEventListener("click", increaseCount)
-decrease.addEventListener("click", decreaseCount)
-reset.addEventListener("click", resetCount)
+
+counterApp()
+
+
+function counterApp() {
+  increase.addEventListener("click", increaseCount)
+  decrease.addEventListener("click", decreaseCount)
+  reset.addEventListener("click", resetCount)
 
 let count = 0
 
-function increaseCount() {
-  countUp = count += 1
-  number.innerText = `${countUp}`
+     function increaseCount() {
+      count += 1
+      updateDisplay()
+    }
+    function decreaseCount() {
+      // Allow negative values to show different UI states (red for negative)
+      count -= 1 
+      /*
+        clamp counter at 0 (no negative values)
+       
+      if (count > 0) { 
+         count -= 1
+     } else {
+             count = 0    
+          }
+      updateDisplay()
+      */
+    }
+    function resetCount() {
+      count = 0
+      updateDisplay()
+    }
+    function countColor() {
+      if (count > 0) {
+        number.style.color = "rgb(0, 184, 0)"
+      } else if (count < 0) {
+        number.style.color = "rgb(255, 0, 0)"
+      }
+        else {
+        number.style.color = "rgb(0, 0, 0)"
+      }
+    }
+    function updateDisplay() {
+      
+      number.innerText = count
+      countColor()
+    } 
 }
-
-function decreaseCount() {
-  countDown = count -= 1
-  number.innerText = `${countDown}`
-}
-
-function resetCount() {
-  countReset = count = 0
-  number.innerText = `${countReset}`
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
