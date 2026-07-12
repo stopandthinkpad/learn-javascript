80d1d000cc0b10f6d27bee05ac05bac93efc37f9
stopandthinkpad <298364173+stopandthinkpad@users.noreply.github.com>
2026-07-12 14:52:44 -0400

    create basic counter app project4_counterapp

---
+++ mini-projects/project4_counterapp/index.js
@@ -0,0 +1,50 @@
+/*this is for use with event listener
+const number = document.getElementById("number")
+const increase = document.getElementById("increase")
+const decrease = document.getElementById("decrease")
+const reset = document.getElementById("reset")
+*/
+let count = 0
+
+function increaseCount() {
+  countUp = count += 1
+  number.innerText = `${countUp}`
+}
+
+function decreaseCount() {
+  countDown = count -= 1
+  number.innerText = `${countDown}`
+}
+
+function resetCount() {
+  countReset = count = 0
+  number.innerText = `${countReset}`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
